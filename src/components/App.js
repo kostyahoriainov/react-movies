3b3369086cac859305f714ef8113ca2a1917562c
kostyahoriainov <kostiantyn.horiainov@gmail.com>
2018-12-07 20:14:03 +0200

    class

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import data from '../data';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="ui container">
+                <h1>Movies</h1>
+            </div>
+        );
+    }
+}
+
+
+export default App;
